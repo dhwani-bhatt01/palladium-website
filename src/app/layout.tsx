d45383f898a582fd978aedc21782a8c4b4d10629
dhwani bhatt <bhattdhwani01@gmail.com>
2024-03-01 23:33:08 +0530

    updated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import glowPng from "@/assets/glow-bg/glow-bg.webp";
 import { cn } from "@/lib/utils";
 import { Metadata } from "next";
 import localFont from "next/font/local";
+import Image from "next/image";
 import "./globals.css";
 
 // const inter = Inter({ subsets: ["latin"] });
@@ -79,7 +80,7 @@ export default function RootLayout({
 	return (
 		<html lang="en">
 			<head>
-				<link rel="icon" href="/favicon.webp" />
+				<link rel="icon" href="/favicon.ico" />
 			</head>
 
 			<body
@@ -88,15 +89,15 @@ export default function RootLayout({
 					"h-screen overflow-y-auto overflow-x-hidden relative bg-[radial-gradient(#5E5222_1.5px,_transparent_1.5px)] [background-size:30px_30px] bg-[#0A0A0A] bg-fixed"
 				)}
 			>
-				<img
+				<Image
 					alt="Glow Background"
-					src={glowPng.src}
+					src={glowPng}
 					className="h-[1600px] z-0 fixed top-[-34%] left-[-44%]"
 				/>
 
-				<img
+				<Image
 					alt="Glow Background"
-					src={glowPng.src}
+					src={glowPng}
 					className="h-[1200px] z-0 fixed bottom-[-63%] right-[-45%]"
 				/>
 
