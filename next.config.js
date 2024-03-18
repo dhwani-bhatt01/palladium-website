cb36553e7f2af0c8dfedc91c14d0fb26539a0041
dhwani bhatt <bhattdhwani01@gmail.com>
2024-03-18 23:14:06 +0530

    added amplify.yml and commented nextconfig code

--- next.config.js
+++ next.config.js
@@ -1,45 +1,45 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" },
-      "/privacy-policy/index": { page: "/privacy-policy" },
-    };
-  },
-  images: {
-    unoptimized: true,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-      config.resolve.fallback.tls = false;
-      config.resolve.fallback.net = false;
-      config.resolve.fallback.child_process = false;
-    }
+	// output: "export",
+	// exportPathMap: async function () {
+	//   return {
+	//     "/": { page: "/" },
+	//     "/privacy-policy/index": { page: "/privacy-policy" },
+	//   };
+	// },
+	images: {
+		unoptimized: true,
+	},
+	webpack: (config, { isServer }) => {
+		if (!isServer) {
+			config.resolve.fallback.fs = false;
+			config.resolve.fallback.tls = false;
+			config.resolve.fallback.net = false;
+			config.resolve.fallback.child_process = false;
+		}
 
-    return config;
-  },
+		return config;
+	},
 
-  // other next config
-  // i18n: {
-  // 	locales: ["en", "ms"],
-  // 	defaultLocale: "en",
-  // },
+	// other next config
+	// i18n: {
+	// 	locales: ["en", "ms"],
+	// 	defaultLocale: "en",
+	// },
 
-  // webpack: (config, options) => {
-  // 	config.node = {
-  // 		// Some libraries import Node modules but don't use them in the browser.
-  // 		// Tell Webpack to provide empty mocks for them so importing them works.
-  // 		...config.node,
-  // 		fs: "empty",
-  // 		child_process: "empty",
-  // 		net: "empty",
-  // 		tls: "empty",
-  // 	};
+	// webpack: (config, options) => {
+	// 	config.node = {
+	// 		// Some libraries import Node modules but don't use them in the browser.
+	// 		// Tell Webpack to provide empty mocks for them so importing them works.
+	// 		...config.node,
+	// 		fs: "empty",
+	// 		child_process: "empty",
+	// 		net: "empty",
+	// 		tls: "empty",
+	// 	};
 
-  // 	return config;
-  // },
+	// 	return config;
+	// },
 };
 
 module.exports = nextConfig;
